cebcc5afd515785fb390f5b64eb6cf58ccc26f11
12521125DinhVietHung125213 <dinhtuankiet2730@gmail.com>
2023-12-10 19:19:57 +0700

    code

---
+++ User/Js/home.js
@@ -0,0 +1,25 @@
+var app = angular.module('AppBanHang', []);
+app.controller("HomeCtrl", function ($scope, $http) {
+    $scope.listMenu;
+	$scope.listItem;	
+    $scope.LoadMenu= function () {
+        $http({
+            method: 'GET',
+            url: current_url + '/api/item-group/get-menu',
+        }).then(function (response) {  
+            $scope.listMenu = response.data;  
+        });
+    };
+	 
+    $scope.GetBanChay= function () {
+        $http({
+            method: 'POST',
+            data: { page: 1, pageSize: 10},
+            url: current_url + '/api/khach/search',
+        }).then(function (response) {  
+            debugger;
+            $scope.listItem = response.data.data;  
+        });
+    };   
+	$scope.GetBanChay();
+});vi
